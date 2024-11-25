6cf766e061678d5268cd48af08a25101719f171e
syhemm <soumeya.tahar@gmail.com>
2024-11-24 23:41:11 -0500

    commit for the streak

--- src/BankingApp.java
+++ src/BankingApp.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 
+//Practice for Java functions. 23-11-2024
 public class BankingApp {
     public static void main(String [] args) {
         Scanner keyboard = new Scanner(System.in);
