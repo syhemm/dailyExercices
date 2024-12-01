5bcdb138c88859849b17f1355c1bae3682125715
syhemm <soumeya.tahar@gmail.com>
2024-11-30 23:05:50 -0500

    commit for streak

--- src/Cryptogramme.java
+++ src/Cryptogramme.java
@@ -1,23 +1,25 @@
 public class Cryptogramme {
     public static void main(String[] args) {
 
+        // char has to be casted for msg.charAt AND the key to return a char
+
         String msg = "Asterix et Obelix";
         int key = 1;
         String msgEncr = "";
         String msgDecode = "";
+        char a = 'a';
 
-        for (int i = 0; i < msg.length(); i++)
-        {
-           msgEncr += (char)(msg.charAt(i) + key);
+        for (int i = 0; i < msg.length(); i++) {
+            msgEncr += (char) (msg.charAt(i) + key);
         }
 
         System.out.println("msg encrypter: " + msgEncr);
 
-        for (int i = 0; i < msgEncr.length(); i++)
-        {
-            msgDecode += (char)(msgEncr.charAt(i) - key);
+        for (int i = 0; i < msgEncr.length(); i++) {
+            msgDecode += (char) (msgEncr.charAt(i) - key);
         }
         System.out.println("msg decode: " + msgDecode);
-    }
 
+        System.out.println((char) (2+a));
+    }
 }
