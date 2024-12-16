57f9c0b50e0cdf1c07f87c47d00668a783c8ee9a
syhemm <soumeya.tahar@gmail.com>
2024-12-15 20:33:47 -0500

    for the streak

--- src/test.java
+++ src/test.java
@@ -1,52 +1,13 @@
 public class test {
     public static void main(String [] args) {
+        String a ="hi";
+        String b="h";
 
-        int nbPositifs = 0;
-        int index = 0;
+        b+="i";
 
-        // A) Déclaration, instanciation et initialisation des tableaux "tab1" et "tab2"
-        int [] tab1 = {-4, 3, -10, 5, 2, 9};
-        int [] tab2 = {2, -3, 6, 5, 1, -9, 7};
+        System.out.println("a = "+a);
+        System.out.println("b = "+b);
 
-        // B) Déclaration et instanciation du tableau "positifs"
-        int [] positifs;
-
-        // C) Initialisation des valeurs du tableau "positifs"
-        for (int i = 0; i < tab1.length; i++)
-        {
-            if (tab1[i] >= 0)
-            {
-                nbPositifs++;
-            }
-        }
-        for (int j = 0; j < tab2.length; j++)
-        {
-            if (tab2[j] >= 0)
-            {
-                nbPositifs++;
-            }
-        }
-        positifs = new int [nbPositifs];
-
-        for (int i = 0; i < tab1.length; i++)
-        {
-            if (tab1[i] >= 0)
-            {
-                positifs[index] = tab1[i];
-                index++;
-            }
-        }
-        for (int i = 0; i < tab2.length; i++)
-        {
-            if (tab2[i] >= 0)
-            {
-                positifs[index] = tab2[i];
-                index++;
-            }
-        }
-        for (int i = 0; i < positifs.length; i++)
-        {
-            System.out.print(positifs[i] + " ");
-        }
+        System.out.println(a==b);
     }
 }
